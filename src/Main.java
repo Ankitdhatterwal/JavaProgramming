f4f2cd9b8726f54498f7ed3f2f855769680e5ed4
Ankit Dhatterwal <90125082+Ankitdhatterwal@users.noreply.github.com>
2023-08-21 08:05:50 +0530

    this is my first edit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a =10;
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}

10c72dde7c37a741d589ade8ed2896910d122733
Ankit Dhatterwal <90125082+Ankitdhatterwal@users.noreply.github.com>
2023-10-30 09:39:35 +0530

    program asked in haxaview

---
+++ src/BasicOfJava/basic/program2.java
@@ -0,0 +1,34 @@
+package BasicOfJava.basic;
+import java.util.Scanner;
+public class program2 {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int arr[] = new int[100];
+        System.out.println("length of the array : ");
+       int n = scan.nextInt();
+       for(int i=0 ; i<n; i++ ){
+            arr[i] = scan.nextInt();
+        }
+        program2.largest(n,arr);
+        smallest(n,arr);
+    }
+    private static void largest(int n, int[] arr) {
+        int largest =arr[0];
+        for(int i =0; i<n; i++){
+            if(largest < arr[i]){
+                largest = arr[i];
+            }}
+        System.out.println(largest);
+
+    }
+    private static void smallest(int n, int[] arr) {
+        int smallest =arr[0];
+        for(int i =0; i<n; i++){
+            if(smallest > arr[i]){
+                smallest = arr[i];
+            }
+        }
+        System.out.println(smallest);
+    }
+
+}

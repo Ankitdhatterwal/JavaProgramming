eec8253d27971e854c9a5e5c35c30ad844b68181
Ankit Dhatterwal <90125082+Ankitdhatterwal@users.noreply.github.com>
2023-10-31 11:17:04 +0530

    program to find missing number

--- src/BasicOfJava/basic/Basics/Main.java
+++ src/BasicOfJava/basic/Basics/Main.java
@@ -2,6 +2,7 @@ package BasicOfJava.basic.Basics;
 
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,13 +21,26 @@ public class Main {
 
 
         //** program To find the prime number between a range
-        System.out.println("Enter the start number :");
-        int n1 = scan.nextInt();
-        System.out.println("Enter the second number :");
-        int n2  = scan.nextInt();
-        FindPrime findPrime =new FindPrime();
+//        System.out.println("Enter the start number :");
+//        int n1 = scan.nextInt();
+//        System.out.println("Enter the second number :");
+//        int n2  = scan.nextInt();
+//        FindPrime findPrime =new FindPrime();
+//
+//        List<Integer>list = findPrime.findPrime(n1,n2);
+//        System.out.println(list);
 
-        List<Integer>list = findPrime.findPrime(n1,n2);
-        System.out.println(list);
-    
+        //** program to find the devident of any given number
+//        System.out.println("Enter the number to check : ");
+//        int n = scan.nextInt();
+//        checkDevidents devidents = new checkDevidents();
+//       List<Integer> list =new ArrayList<>();
+//       list=  devidents.checkD(n);
+//        System.out.println(list);
+
+// program to print patten
+
+        Pattern pattern  = new Pattern();
+        pattern.patter(8);
+
 }}
